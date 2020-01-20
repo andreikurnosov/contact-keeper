d2f3b1757df95f8c1bc22366184f25dfc4a0046d
nixdie <nixdie@yandex.ru>
2020-01-20 23:57:16 +0700

    delete contact from ui

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -36,12 +36,18 @@ const ContactState = props => {
   const addContact = contact => {
     contact.id = uuid.v4();
     dispatch({ type: ADD_CONTACT, payload: contact })
+  }  
+
+  const deleteContact = id => {
+    dispatch({ type: DELETE_CONTACT, payload: id })
   }
+
   return (
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
-        addContact
+        addContact,
+        deleteContact
       }}
     >
       { props.children }
