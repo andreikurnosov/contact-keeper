0cd8c431b4f57744e47e2c859e91a2ab4f9c144d
nixdie <nixdie@yandex.ru>
2020-01-21 10:47:31 +0700

    auth context initial state

--- client/src/components/contacts/Contacts.jsx
+++ client/src/components/contacts/Contacts.jsx
@@ -1,4 +1,6 @@
 import React, { Fragment, useContext } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
 import ContactItem from './ContactItem';
 import ContactContext from '../../context/contact/contactContex';
 
@@ -7,17 +9,23 @@ const Contacts = () => {
 
   const { contacts, filtered } = contactContext;
 
-  if (contacts.lenght === 0) {
+  if (contacts.length === 0) {
     return <h4>Please add a contact</h4>
   }
 
   return (
     <Fragment>
-      {filtered !== null ? filtered.map(contact => (
-        <ContactItem key={contact.id} contact={contact} />
-      )) : contacts.map(contact => (
-        <ContactItem key={contact.id} contact={contact} />
-      ))}
+      <TransitionGroup>
+        {filtered !== null ? filtered.map(contact => (
+          <CSSTransition  key={contact.id} timeout={500} classNames="item">
+            <ContactItem contact={contact} />
+          </CSSTransition>
+        )) : contacts.map(contact => (
+          <CSSTransition  key={contact.id} timeout={500} classNames="item">
+            <ContactItem contact={contact} />
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
     </Fragment>
   )
 }
