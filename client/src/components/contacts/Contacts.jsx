c3fc416d26e66d52a2bf6e9b49aa65279e65356c
nixdie <nixdie@yandex.ru>
2020-01-21 10:11:30 +0700

    added contact filter from state

--- client/src/components/contacts/Contacts.jsx
+++ client/src/components/contacts/Contacts.jsx
@@ -1,17 +1,23 @@
-import React, { Fragment, useContext} from 'react';
+import React, { Fragment, useContext } from 'react';
 import ContactItem from './ContactItem';
 import ContactContext from '../../context/contact/contactContex';
 
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
 
-  const { contacts } = contactContext;
+  const { contacts, filtered } = contactContext;
+
+  if (contacts.lenght === 0) {
+    return <h4>Please add a contact</h4>
+  }
 
   return (
     <Fragment>
-      {contacts.map(contact => (
+      {filtered !== null ? filtered.map(contact => (
         <ContactItem key={contact.id} contact={contact} />
-      ))}      
+      )) : contacts.map(contact => (
+        <ContactItem key={contact.id} contact={contact} />
+      ))}
     </Fragment>
   )
 }
