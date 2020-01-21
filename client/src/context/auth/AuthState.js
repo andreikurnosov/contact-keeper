9c7f102b59a404ccc9d255e0c118b09d20bfc119
nixdie <nixdie@yandex.ru>
2020-01-21 23:39:01 +0700

    user login

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -66,6 +66,31 @@ const AuthState = props => {
     }
   }
 
+
+  const login = async (formData) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    try {
+      const res = await axios.post('/api/auth', formData, config);
+      
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data
+      });
+
+      loadUser();
+    } catch (err) {
+        dispatch({
+          type: LOGIN_FAIL,
+          payload: err.response.data.msg
+        })    
+    }
+  }
+
   const clearErrors = () => {
     dispatch({ type: CLEAR_ERRORS })
   }
@@ -81,7 +106,8 @@ const AuthState = props => {
         error: state.error,
         register,
         clearErrors,
-        loadUser
+        loadUser,
+        login
       }}
     >
       { props.children }
