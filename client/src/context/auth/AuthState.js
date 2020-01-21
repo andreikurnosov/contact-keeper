487d0a20379e601a936f8edf4602c1da19e47d9a
nixdie <nixdie@yandex.ru>
2020-01-21 23:27:05 +0700

    load user set token

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -2,6 +2,7 @@ import React, { useReducer } from 'react';
 import axios from 'axios';
 import AuthContext from './authContext';
 import authReducer from './authReducer';
+import setAuthToken from '../../utils/setAuthToken';
 import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
@@ -23,7 +24,24 @@ const AuthState = props => {
   }
 
   const [state, dispatch] = useReducer(authReducer, initialState);
-  
+
+  const loadUser = async () => {
+    if(localStorage.token) {
+      setAuthToken(localStorage.token)
+    }
+    
+    try {
+      const res = await axios.get('/api/auth');
+
+      dispatch({
+        type: USER_LOADED, 
+        payload: res.data
+      });
+    } catch (err) {
+      dispatch({type: AUTH_ERROR})
+    }
+  }
+
   const register = async (formData) => {
     const config = {
       headers: {
@@ -37,7 +55,9 @@ const AuthState = props => {
       dispatch({
         type: REGISTER_SUCCESS,
         payload: res.data
-      })      
+      });
+
+      loadUser();
     } catch (err) {
         dispatch({
           type: REGISTER_FAIL,
@@ -50,6 +70,7 @@ const AuthState = props => {
     dispatch({ type: CLEAR_ERRORS })
   }
 
+
   return (
     <AuthContext.Provider
       value={{
@@ -59,7 +80,8 @@ const AuthState = props => {
         user: state.user,
         error: state.error,
         register,
-        clearErrors
+        clearErrors,
+        loadUser
       }}
     >
       { props.children }
